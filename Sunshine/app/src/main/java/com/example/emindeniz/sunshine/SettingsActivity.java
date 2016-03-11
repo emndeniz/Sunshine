5830fb2d5409992b34abc61b1bc8538b308f9d28
medeniz <medeniz@netas.com.tr>
2016-03-11 11:45:48 +0200

    Settings activity added. Settings added for Main and Detailed activity. Colors updated Preferences xml file updated as pref_general

--- Sunshine/app/src/main/java/com/example/emindeniz/sunshine/SettingsActivity.java
+++ Sunshine/app/src/main/java/com/example/emindeniz/sunshine/SettingsActivity.java
@@ -22,12 +22,12 @@ public class SettingsActivity extends PreferenceActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Add 'general' preferences, defined in the XML file
-        // TODO: Add preferences from XML
-        addPreferencesFromResource(R.xml.preferences);
+        // Add 'general' pref_general, defined in the XML file
+
+        addPreferencesFromResource(R.xml.pref_general);
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
-        // TODO: Add preferences
+        // TODO: Add pref_general
     }
 
     /**
